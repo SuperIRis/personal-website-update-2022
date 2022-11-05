7690519672a1e441eed90330c45b7e359f10dd1b
Iris <iris@element.com.mx>
2022-11-04 21:17:01 -0700

    Implemented gulp build and finished gulp serve with css

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,13 @@
 "use strict";
 
 // Include Gulp & Tools We'll Use
-/*var gulp = require("gulp");
-var $ = require("gulp-load-plugins")();
-var sass = require("gulp-ruby-sass");
-//var del = require('del');
-var runSequence = require("run-sequence");
-var browserSync = require("browser-sync");
-var pagespeed = require("psi");
-var reload = browserSync.reload;
-var fs = require("fs");
-const browserify = require("gulp-browserify");
-const buffer = require("vinyl-buffer");
-const source = require("vinyl-source-stream");
-*/
+const uglify = require("gulp-uglify");
+const sass = require("gulp-sass")(require("sass"));
 const browserSync = require("browser-sync").create();
 const rename = require("gulp-rename");
 const imagemin = require("gulp-imagemin");
 const babel = require("gulp-babel");
 const { series, dest, src, parallel, watch } = require("gulp");
-
-var AUTOPREFIXER_BROWSERS = [
-	"ie >= 10",
-	"ie_mob >= 10",
-	"ff >= 30",
-	"chrome >= 34",
-	"safari >= 7",
-	"opera >= 23",
-	"ios >= 7",
-	"android >= 4.4",
-	"bb >= 10",
-];
 
 function browserSyncServe() {
 	browserSync.init({
@@ -42,18 +19,40 @@ function browserSyncServe() {
 		processJSDev();
 		browserSync.reload();
 	});
+	watch("./app/sass/**/*.scss").on("change", processCSSDev);
 }
 
 const processJSDev = () => {
-	console.log("process JS");
 	return src("./app/js/main.js")
 		.pipe(babel())
 		.pipe(rename("bundle2.js"))
 		.pipe(dest("./app/js/", { overwrite: true }));
 };
 
-const copyHTML = () => src("./app/*.html").pipe(dest("./dist/"));
+const processJSProd = () => {
+	return src("./app/js/main.js")
+		.pipe(babel())
+		.pipe(rename("bundle2.js"))
+		.pipe(uglify())
+		.pipe(dest("./dist/js/", { overwrite: true }));
+};
 
+const processCSSDev = () => {
+	console.log("css");
+	return src("./app/sass/main.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(dest("./app/css/"), { overwrite: true })
+		.pipe(browserSync.stream({ match: "**/*.css" }));
+};
+
+const processCSSProd = () => {
+	return src("./app/sass/**/*.scss")
+		.pipe(sass.sync({ outputStyle: "compressed" }).on("error", sass.logError))
+		.pipe(dest("./dist/css/"), { overwrite: true })
+		.pipe(browserSync.stream({ match: "**/*.css" }));
+};
+
+const copyHTML = () => src("./app/*.html").pipe(dest("./dist/"));
 const copyData = () => src("./app/data/*").pipe(dest("./dist/data/"));
 const copyDocs = () => src("./app/docs/*").pipe(dest("./dist/docs/"));
 const copyIcon = () => src("./app/*.{ico,txt}").pipe(dest("./dist/"));
@@ -61,14 +60,16 @@ const copyServerCode = () => src("./app/process/*").pipe(dest("./dist/process/")
 const copySpritesheets = () => src("./app/spritesheets/*").pipe(dest("./dist/spritesheets/"));
 const optimizeImages = () => src("./app/images/**/*").pipe(imagemin()).pipe(dest("./dist/images"));
 
-exports.serve = series(processJSDev, browserSyncServe);
+exports.serve = series(processJSDev, processCSSDev, browserSyncServe);
 
-exports.build = series(
+exports.build = parallel(
 	copyHTML,
 	copyData,
 	copyDocs,
 	copySpritesheets,
 	copyIcon,
 	copyServerCode,
-	optimizeImages
+	optimizeImages,
+	processCSSProd,
+	processJSProd
 );
